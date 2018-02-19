dd1d38f6b2838f14ee252c4554e242ab19aaf934
Varun <vnr2friends@gmail.com>
2018-02-19 10:10:52 +0530

    eslint bugs fixed

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,10 +1,9 @@
 // Requring plugins here
 // import '../../node_modules/aos/src/sass/aos.scss';
 
-import $ from 'jquery';
+import 'jquery';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Modernizr from 'modernizr';
-
+import 'modernizr';
 
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
